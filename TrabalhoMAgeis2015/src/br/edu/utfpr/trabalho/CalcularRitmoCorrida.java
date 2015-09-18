584abb9d98366c4ec9ad89995a686b996f5d752b
viniciusfu <viniciusfu@gmail.com>
2015-09-17 22:29:01 -0300

    Create CalcularRitmoCorrida.java

---
+++ TrabalhoMAgeis2015/src/br/edu/utfpr/trabalho/CalcularRitmoCorrida.java
@@ -0,0 +1,8 @@
+package br.edu.utfpr.trabalho;
+
+public class CalcularRitmoCorrida {
+
+	public int calcularRitmoKM(int tempoEmSegundos, int distanciaEmKm){
+		return tempoEmSegundos/distanciaEmKm;
+	}
+}
